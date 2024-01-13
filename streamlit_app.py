2546624e9e9ee394409986fb574a322baf170226
slmogz <150812271+slmogz@users.noreply.github.com>
2024-01-13 21:07:19 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.title('My parants new healthy dinner')
+streamlit.title('My parents new healthy dinner')
 streamlit.header('🍕Breakfast Menu')
 streamlit.text('🍓Omega 3 & Blueberry Oatmeal')
 streamlit.text('🍸Kale, Spinach & Rocket Smoothie')
