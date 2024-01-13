3f4ae795ee45bd3cb9f4907f758e9d5ede035e6c
slmogz <150812271+slmogz@users.noreply.github.com>
2024-01-13 21:06:07 +0300

    Update streamlit_app.py
    Emoji added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My parants new healthy dinner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('🍕Breakfast Menu')
+streamlit.text('🍓Omega 3 & Blueberry Oatmeal')
+streamlit.text('🍸Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥚Hard-Boiled Free-Range Egg')
