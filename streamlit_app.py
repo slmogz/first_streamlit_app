82d1046046d499474032ff3610bd6f2ad4767501
slmogz <150812271+slmogz@users.noreply.github.com>
2024-01-13 21:09:57 +0300

    csv added

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ streamlit.header('🍕Breakfast Menu')
 streamlit.text('🍓Omega 3 & Blueberry Oatmeal')
 streamlit.text('🍸Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥚Hard-Boiled Free-Range Egg')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
