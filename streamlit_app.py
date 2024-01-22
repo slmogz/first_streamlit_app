40ab5162a40ff4eea700f9a07223d5632c195752
slmogz <150812271+slmogz@users.noreply.github.com>
2024-01-22 12:16:08 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,11 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
+
+import snowflake.connector
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
